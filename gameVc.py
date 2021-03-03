984e72d1013d2805ad2819096a68e16c3efa3427
sopheasuon <sophea.suon@student.passerellesnumeriques.org>
2021-03-03 08:14:28 +0700

    #3 move box

--- gameVc.py
+++ gameVc.py
@@ -5,7 +5,15 @@ import tkinter as tk
 # CONSTANTS
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
-SCREEN_TITLE = "PNC Data structure to graphism"
+root = tk.Tk()  
+# frame = tk.Frame()
+# frame.master.title("Sophea")
+root.geometry(str(SCREEN_WIDTH)+"x"+str(SCREEN_HEIGHT))
+canvas = tk.Canvas(root, bg="white")
+# imageS = tk.PhotoImage(file="Sky.png")
+imageF = tk.PhotoImage(file="female1.png")
+imageW = tk.PhotoImage(file="Wall.png")
+imageB = tk.PhotoImage(file="Box.png")
 
 
 # VARIABLES
@@ -25,12 +33,8 @@ grid =[
 ]
 index = 0
 
-# squareSize = #choose the appropriate size of the squares
 
-
-# VARIABLES
 def arrayToDrawing(grid):
-    # global image
     for n in range (len(grid)):
         for i in range (len(grid[n])):
             x0 = i*50
@@ -38,7 +42,7 @@ def arrayToDrawing(grid):
             x1 = x0+50
             y1 = y0+50
             if grid[n][i] == 1:
-                canvas.create_image(x0+20, y0+20,image=imageF)
+                canvas.create_image(x0+25, y0+20,image=imageF)
             elif grid[n][i] == 2:
                 canvas.create_image(x0+25, y0+25, image=imageW)
             elif grid[n][i] == 3:
@@ -46,9 +50,13 @@ def arrayToDrawing(grid):
             elif grid[n][i] == "b":
                 canvas.create_image(x0+25, y0+25, image=imageB)
             else:
+                # canvas.create_image(x0+25, y0+25, image=imageS)
                 canvas.create_rectangle(x0, y0, x1, y1, fill="white", outline="white")
 
 def getPlayerPosition(grid):
+    global imageS
+    # canvas.create_image(0, 0, image=imageS)
+    canvas.delete("all")
     for n in range (len(grid)):
         for i in range (len(grid[n])):
             if grid[n][i] == 1:
@@ -61,77 +69,70 @@ def left(event):
     global grid
     row = getPlayerPosition(grid)[0]
     col = getPlayerPosition(grid)[1]
-    if grid[row][col-1] != 2:
-        grid[row][col] = 0
-        grid[row][col-1] = 1 
-        
-    elif grid[row][col-1]== "b":
-        grid[row][col] = 0
-        grid[row][col-1] = 1
-        grid[row][col-2] = "b"
-    arrayToDrawing(grid)
-    
-       
+    if col > 0:
+        if grid[row][col-1] == 0:
+            grid[row][col] = 0
+            grid[row][col-1] = 1
+        elif grid[row][col-1] == "b" and col > 0:
+            if grid[row][col-2] == 0:
+                grid[row][col-2] = "b"
+                grid[row][col] = 0
+                grid[row][col-1] = 1        
+        arrayToDrawing(grid)
 
-      
+     
 def right(event):
     global grid
     row = getPlayerPosition(grid)[0]
     col = getPlayerPosition(grid)[1]
-    if grid[row][col+1] != 2:
-        grid[row][col] = 0
-        grid[row][col+1] = 1 
-        
-    elif grid[row][col+1] == "b":
-        grid[row][col] = 0
-        grid[row][col+1] = 1
-        grid[row][col+2] = "b" 
-    arrayToDrawing(grid)
+    if col > 0:
+        if grid[row][col+1] == 0:
+            grid[row][col] = 0
+            grid[row][col+1] = 1
+        elif grid[row][col+1] == "b" and col >0:
+            if grid[row][col+2] == 0:
+                grid[row][col+2] = "b"
+                grid[row][col] = 0
+                grid[row][col+1] = 1         
+        arrayToDrawing(grid)
 
 def up(event):
     global grid
     row = getPlayerPosition(grid)[0]
     col = getPlayerPosition(grid)[1]
-    if grid[row-1][col] != 2:       
-        grid[row][col] = 0
-        grid[row-1][col] = 1 
-        
-    elif grid[row-1][col] == "b":
-        grid[row][col] = 0
-        grid[row-1][col] = 1 
-        grid[row-2][col] = "b"
-    arrayToDrawing(grid)
-    
-        
+    if row > 0:
+        if grid[row-1][col] == 0:
+            grid[row][col] = 0
+            grid[row-1][col] = 1
+        elif grid[row-1][col] == "b" and row > 0:
+            if grid[row-2][col] == 0:
+                grid[row-2][col] = "b"
+                grid[row][col] = 0
+                grid[row-1][col] = 1    
+        arrayToDrawing(grid)
+
 
 def down(event):
     global grid
     row = getPlayerPosition(grid)[0]
     col = getPlayerPosition(grid)[1]
-    if grid[row+1][col] != 2:
-        grid[row][col] = 0
-        grid[row+1][col] = 1 
-        
-    elif grid[row+1][col] == "b":
-        grid[row][col] = 0
-        grid[row+1][col] = 1 
-        grid[row+2][col] = "b" 
-    arrayToDrawing(grid)
+    if row > 0:
+        if grid[row+1][col] == 0:
+            grid[row][col] = 0
+            grid[row+1][col] = 1
+        elif grid[row+1][col] == "b" and row > 0:
+            if grid[row+2][col] == 0:
+                grid[row+2][col] = "b"
+                grid[row][col] = 0
+                grid[row+1][col] = 1   
+        arrayToDrawing(grid)
 
-        
-
-        
-# draw a line with white and black squares using the global array
-root = tk.Tk()
-imageF = tk.PhotoImage(file="female(1).png")
-imageW = tk.PhotoImage(file="wall.png")
-imageB = tk.PhotoImage(file="box.png")
-# imageB = tk.PhotoImage(file="box.png")
-root.geometry(str(SCREEN_WIDTH)+"x"+str(SCREEN_HEIGHT))
-canvas = tk.Canvas(root,bg="white")
-
-# root.resizable(False, False)
+     
+root.resizable(False, False)
 canvas.pack(expand=True, fill="both")
+# frame.pack(expand=True, fill="both")
+
+
 root.bind("<Left>", left)
 root.bind("<Right>", right)
 root.bind("<Up>", up)
